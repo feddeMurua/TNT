602de557e0990952aebe7ada68a4ce809041b644
Fedde Murua <fedde.murua@gmail.com>
2018-09-01 13:19:48 -0300

    Toast Message con Imagen

--- app/src/main/java/murua/fedde/prueba/MainActivity.java
+++ app/src/main/java/murua/fedde/prueba/MainActivity.java
@@ -2,9 +2,13 @@ package murua.fedde.prueba;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -19,11 +23,32 @@ public class MainActivity extends Activity {
         button = findViewById(R.id.mainbutton);
 
         button.setOnClickListener(new OnClickListener() {
-
+            /*
             @Override
             public void onClick(View arg0) {
-
+                //muestra solo un mensaje
                 Toast.makeText(getApplicationContext(),"Mensaje Toast", Toast.LENGTH_LONG).show();
+            }
+            */
+
+            @Override
+            public void onClick(View view) {
+                //muestra mensaje con una imagen
+                // get your toast.xml layout
+                LayoutInflater inflater = getLayoutInflater();
+
+                View layout = inflater.inflate(R.layout.toast,(ViewGroup) findViewById(R.id.toast_layout_id));
+
+                // set a message
+                TextView text = (TextView) layout.findViewById(R.id.text);
+                text.setText("Mensaje Toast con imagen");
+
+                // Toast configuration
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
+                toast.show();
             }
         });
     }
